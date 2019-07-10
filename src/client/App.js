d7d040413d7a5765ef87c1029d4b64b8c234ef29
Matthew Puku <matthew.puku@gmail.com>
2019-07-10 22:59:11 +1000

    connected database to frontend

--- src/client/App.js
+++ src/client/App.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import './app.css';
 
 export default class App extends Component {
-  state = { message: null };
+  state = { message: null, products: [] };
 
   componentDidMount() {
     fetch('/api/getMessage')
       .then(res => res.json())
       .then(res => this.setState({ message: res.message }));
+    fetch('api/products')
+      .then(res => res.json())
+      .then(res => this.setState({ products: res }))
+    console.log(this.state.products);
   }
 
   render() {
-    const { message } = this.state;
+    const { message, products } = this.state;
     return (
       <div>
         {<h1>{`The server says: ${message}`}</h1>}
+        <p>This is the first product in the collection: {products[0] && products[0].name}</p>
+        <button onClick={() => console.log(products)}>Console log all products</button>
       </div>
     );
   }
