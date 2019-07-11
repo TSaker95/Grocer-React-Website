dee5c789481d0ebe5f79856d7739915b7c37ceca
RhyG <rhysgeary@gmail.com>
2019-07-11 16:08:38 +1000

    Pre-restructure commit

--- src/client/src/components/dashboard/Dashboard.js
+++ src/client/src/components/dashboard/Dashboard.js
@@ -5,31 +5,32 @@ import ProductList from "./ProductList";
 import SpecialsList from "./SpecialsList";
 
 export default function Dashboard() {
-  const [state, setState] = useState({});
+  const [products, setProducts] = useState([]);
+  const [specials, setSpecials] = useState([]);
 
   useEffect(() => {
     api
       .get("api/products")
       .then(res => {
-        setState({ products: [...res.data] });
+        setProducts(...res.data);
       })
       .catch(err => console.log(`Error: ${err}`));
 
     api
       .get("/api/specials")
       .then(res => {
-        let prevState = state;
-        console.log(prevState);
-        setState({ ...prevState, specials: [...res.data] });
+        setSpecials([...res.data]);
       })
       .catch(err => console.log(`Error: ${err}`));
+
+    console.log(products, specials);
   }, []);
 
   return (
     <div className="container">
       <DashboardNavbar />
-      <SpecialsList specials={state.specials ? state.specials : []} />
-      <ProductList products={state.products ? state.products : []} />
+      {/* <SpecialsList specials={state.specials ? state.specials : []} />
+      <ProductList products={state.products ? state.products : []} /> */}
     </div>
   );
 }
