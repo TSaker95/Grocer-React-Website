162fe3f197dbe93888f09a562feed59db1415321
RhyG <rhysgeary@gmail.com>
2019-07-12 13:04:46 +1000

    Fix data in specials table

--- src/client/components/dashboard/SpecialsItem.js
+++ src/client/components/dashboard/SpecialsItem.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function SpecialsItem(props) {
-  return <li className="specials-item">{props.item.productId}</li>;
+  const findItemById = itemId => {
+    // Find the item from the database
+  };
+
+  return (
+    // const item = findItemById()
+    <div className="specials-item list-row">
+      <p>{props.item._id}</p>
+      <p>Item normal price</p>
+      <p>Item sale price</p>
+      <p>23/02/1903</p>
+      <p>23/02/2903</p>
+      <p>Actions go here</p>
+    </div>
+  );
 }
--- src/client/components/dashboard/SpecialsList.js
+++ src/client/components/dashboard/SpecialsList.js
@@ -3,15 +3,43 @@ import SpecialsItem from "./SpecialsItem";
 
 export default function SpecialsList(props) {
   return (
-    <div className="specials-container">
+    <div className="products-container">
       <h3>Specials ({props.specials.length})</h3>
-      <div className="specials-list">
-        <ul>
-          {props.specials.map(special => {
-            return <SpecialsItem key={special.productId} item={special} />;
-          })}
-        </ul>
+      <div className="specials-list items-list">
+        <div className="headings specials-headings">
+          <h5>Name</h5>
+          <h5>Normal price</h5>
+          <h5>Sale price</h5>
+          <h5>Start date</h5>
+          <h5>End date</h5>
+          <h5>Actions</h5>
+        </div>
+        <div className="list-content specials-list-content">
+          {props.specials.map(special => (
+            <SpecialsItem item={special} />
+          ))}
+        </div>
       </div>
     </div>
   );
+}
+
+{
+  /* <div className="products-container">
+  <h3>Products ({props.products.length})</h3>
+  <div className="products-list items-list">
+    <div className="headings products-headings">
+      <h5>Image</h5>
+      <h5>Name</h5>
+      <h5>Price</h5>
+      <h5>Last sale</h5>
+      <h5>Actions</h5>
+    </div>
+    <div className="list-content products-list-content">
+      {props.products.map(product => (
+        <ProductItem item={product} />
+      ))}
+    </div>
+  </div>
+</div>; */
 }
