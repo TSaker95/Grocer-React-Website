b4ad6553538214d4ba8c48dc97fbf23e43da99cc
TSaker95 <torryarno@hotmail.com>
2019-07-15 14:32:23 +1000

    changes for today

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -1,5 +1,31 @@
 import React from "react";
+import Navbar from "./layout/Navbar.js";
+import logo from "../public/images/hgimage.png";
 
 export default function Main() {
-  return <div>You are on the home page of the website.</div>;
+  return (
+    <div className="pagemargin">
+      <Navbar />
+
+      <div className="hpsection">
+        <img src={logo} alt="logo" />
+      </div>
+
+      <div className="hpsection">
+        <h1> Our Range </h1>
+      </div>
+
+      <div className="hpsection">
+        <h1> Find Us </h1>
+      </div>
+
+      <div className="hpsection">
+        <h1> Specials </h1>
+      </div>
+
+      <div className="hpsection">
+        <h1> Get In Touch </h1>
+      </div>
+    </div>
+  );
 }
--- src/client/components/layout/Navbar.js
+++ src/client/components/layout/Navbar.js
@@ -1,5 +1,36 @@
 import React from "react";
+import logo from "../../public/images/hillsdongrocer_logo.png";
 
 export default function Navbar() {
-  return <div>This is a navbar.</div>;
+  return (
+    <div>
+      <ul className="nav1">
+        <li class="navtext">
+          <a href="#">
+            Our Range <h1> </h1>
+          </a>
+        </li>
+        <li class="navtext">
+          <a href="#">
+            Find Us <h1> </h1>
+          </a>
+        </li>
+
+        <li>
+          <img src={logo} alt="logo" />
+        </li>
+
+        <li class="navtext">
+          <a href="#">
+            Specials<h1> </h1>
+          </a>
+        </li>
+        <li class="navtext">
+          <a href="#">
+            Get In Touch<h1> </h1>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
