a036881467e2418f5f07e29dae2991d72be33c6d
Matthew Puku <matthew.puku@gmail.com>
2019-07-19 11:45:45 +1000

    added tests for get / and get /:id

--- src/server/routes/products.test.js
+++ src/server/routes/products.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const request = require('supertest');
 const app = require('../app');
+const Product = require('../models/product.model');
 
 // May require additional time for downloading MongoDB binaries
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
@@ -14,6 +15,10 @@ beforeAll(async () => {
   await mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
     if (err) console.error(err);
   });
+  await Product.insertMany([
+    { name: 'Dummy', description: 'For babies', price: 3.23 },
+    { name: 'Thiccend Cream', description: 'For pavlova', price: 1.99 },
+  ]);
 });
 
 afterAll(async () => {
@@ -23,7 +28,7 @@ afterAll(async () => {
 
 describe('Product router responds correctly to valid requests', () => {
   it('Creates a product in response to POST "/"', async () => {
-    const product = {
+    const newProduct = {
       name: 'Cheese',
       description: 'Favoured by Wallace and Gromit',
       price: 1.55,
@@ -31,13 +36,32 @@ describe('Product router responds correctly to valid requests', () => {
 
     const res = await request(app)
       .post('/api/products/')
-      .send(product)
+      .send(newProduct)
       .set('Accept', 'application/json');
 
-    if (res.status === 400) {
-      console.log(res.body);
-    }
+    expect(res.status).toEqual(200);
+    expect(res.body).toMatchObject(newProduct);
+  });
+
+  it('Returns all products in response to GET "/"', async () => {
+    const allProducts = await Product.find();
+
+    const res = await request(app)
+      .get('/api/products');
 
     expect(res.status).toEqual(200);
+    expect(JSON.stringify(res.body)).toEqual(JSON.stringify(allProducts));
+  });
+
+  it('Returns one product by id in response to GET "/:id"', async () => {
+    const allProducts = await Product.find();
+    const productID = allProducts[0]._id;
+    const foundProduct = await Product.findById(productID);
+
+    const res = await request(app)
+      .get(`/api/products/${productID}`);
+
+    expect(res.status).toEqual(200);
+    expect(JSON.stringify(res.body)).toEqual(JSON.stringify(foundProduct));
   });
 });
