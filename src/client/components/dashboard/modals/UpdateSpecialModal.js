83c330e613b48df176de2427d9181619ff0f27b9
RhyG <rhysgeary@gmail.com>
2019-07-21 11:08:50 +1000

    Added styles to edit special modal.

--- src/client/components/dashboard/modals/UpdateSpecialModal.js
+++ src/client/components/dashboard/modals/UpdateSpecialModal.js
@@ -4,8 +4,8 @@ import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
-// New style definitions required for reaat modal
-Modal.defaultStyles = {
+// New style definitions required for react-modal
+const styles = {
   overlay: {
     position: "fixed",
     top: 0,
@@ -18,8 +18,8 @@ Modal.defaultStyles = {
     position: "absolute",
     border: "1px solid #ccc",
     borderTop: "6px solid #40A4F4",
-    width: "500px",
-    height: "200px",
+    width: "350px",
+    height: "400px",
     margin: "auto",
     top: "40px",
     left: "40px",
@@ -30,7 +30,7 @@ Modal.defaultStyles = {
     WebkitOverflowScrolling: "touch",
     borderRadius: "4px",
     outline: "none",
-    padding: "0"
+    padding: "20px"
   }
 };
 
@@ -63,25 +63,9 @@ const UpdateSpecialModal = props => {
       shouldCloseOnEsc={true}
       shouldFocusAfterRender={true}
       shouldReturnFocusAfterClose={true}
+      style={styles}
     >
-      <div className="edit-special-modal modal">
-        <h4>{props.item.name}</h4>
-        <form className="special-edit form" onSubmit={updateSpecial}>
-          <input
-            name="price"
-            ref={priceRef}
-            type="text"
-            placeholder="Sale price"
-          />
-          <input name="start-date" ref={startDateRef} type="date" />
-          <input name="end-date" ref={endDateRef} type="date" />
-          <button type="submit">Update special</button>
-        </form>
-
-        <button onClick={props.closeModal}>Close {props.item.name}</button>
-      </div>
-
-      {/* <div className="new-item-modal">
+      <div className="new-item-modal">
         <div className="new-item-title">
           <h4>Update special</h4>
           <p onClick={props.closeModal}>X</p>
@@ -89,14 +73,13 @@ const UpdateSpecialModal = props => {
         <form onSubmit={updateSpecial}>
           <ul className="new-item-form">
             <li>
-              <label htmlFor="">Price</label>
-              <select name="price" required ref={priceRef}>
-                {props.products.map(product => (
-                  <option value={product._id} key={product._id}>
-                    {product.name}
-                  </option>
-                ))}
-              </select>
+              <label htmlFor="price">Sale price</label>
+              <input
+                name="price"
+                ref={priceRef}
+                type="text"
+                placeholder="Sale price"
+              />
             </li>
             <li>
               <label htmlFor="start-date">Start date</label>
@@ -111,12 +94,12 @@ const UpdateSpecialModal = props => {
                 Cancel
               </p>
               <button className="btn btn-primary" type="submit">
-                Add special
+                Update special
               </button>
             </li>
           </ul>
         </form>
-      </div> */}
+      </div>
     </Modal>
   );
 };
