d66d441649ac01f375ead6ae0897ca4116778913
RhyG <rhysgeary@gmail.com>
2019-07-22 13:07:08 +1000

    Styled 404 page.

--- src/client/components/NotFound.js
+++ src/client/components/NotFound.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
-  return <div>Not Found</div>;
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h3>Page not found.</h3>
+      <div className="return-home">
+        <Link to="/">Return home</Link>
+        <p>→</p>
+      </div>
+    </div>
+  );
 }
