7c03f28c4e1e990f5b109fc510faa15c32ee97e6
Matthew Puku <matthew.puku@gmail.com>
2019-07-23 17:27:37 +1000

    auth testing complete

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,39 +1,38 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/user.model'); // Import user model
+const User = require('../models/user.model');
 const checkAuth = require('../middleware/checkAuth');
+const wrap = require('../helpers/wrap');
+const sendError = require('../middleware/sendError');
 
 // @route POST api/auth/login
 // @desc checks given username and password. Signs jwt token if correct
-router.route('/login').post((req, res) => {
+router.post('/login', wrap(async (req, res) => {
   const { username, password } = req.body;
+  const userArray = await User.find({ username });
+  const user = userArray[0];
 
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      res.status(400).json(err);
-    } else if (!user) {
-      res.status(401).json({ error: 'Invalid username or password' });
-    } else {
-      user.checkPassword(password, (invalid, match) => {
-        if (invalid) {
-          res.status(500).json(`Error: ${invalid}`);
-        }
-        if (match) {
-          const payload = { username };
-          const token = jwt.sign(payload, process.env.JWT_SECRET, {
-            expiresIn: '72h',
-          });
-          res.cookie('token', token, { httpOnly: true }).sendStatus(200);
-        } else {
-          res.status(401).json({ error: 'Invalid username or password' });
-        }
-      });
-    }
-  });
-});
+  if (user) {
+    user.checkPassword(password, (invalid, match) => {
+      if (invalid) {
+        res.status(500).json(`Error: ${invalid}`);
+      } else if (match) {
+        const payload = { username };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '72h' });
+        res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+      } else {
+        res.status(401).json({ error: 'Invalid username or password' });
+      }
+    });
+  } else {
+    res.status(401).json({ error: 'Invalid username or password' });
+  }
+}));
 
 // @route GET api/auth/check
 // @desc checks jwt token, sends a 200 if valid or a 401 if token invalid or missing.
-router.get('/check', checkAuth, (req, res) => res.sendStatus(200));
+router.get('/check', checkAuth, wrap(async (req, res) => res.sendStatus(200)));
+
+router.use(sendError);
 
 module.exports = router;
