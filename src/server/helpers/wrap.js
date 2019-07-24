be306d855ae01018b775d081440fe67565be9f0e
Matthew Puku <matthew.puku@gmail.com>
2019-07-24 23:22:16 +1000

    comments, line breaks, and linting, oh my

--- src/server/helpers/wrap.js
+++ src/server/helpers/wrap.js
@@ -1,8 +1,8 @@
-// Wrapping anonymous route functions with this function allows express's error-handling
-// functionality to work properly with asynchonous code. Express already properly handles
-// errors thrown in synchronous code without additional configuration.
+// Wrapping route callbacks in this function allows express's error-handling functionality
+// to work properly with asynchonous code. Express already properly handles errors thrown
+// in synchronous code without additional configuration or a helper like this.
 //
-// Unless you have good reason not to, wrap every route in this function.
+// Unless you have good reason not to, wrap every route callback in this function.
 
 const wrap = fn => (...args) => {
   fn(...args).catch(args[2]);
